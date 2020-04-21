2d7aada8893b26b31138dec03cf0378799a9ed93
Mateus <38238358+byuwur@users.noreply.github.com>
2020-04-20 23:25:55 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap core CSS -->
+<link href="bootstrap-material/css/bootstrap-material-design.css" rel="stylesheet" />
+<link href="css/main.css" rel="stylesheet" />
+<link href="fontawesome/css/all.min.css" rel="stylesheet" />
+<script src="js/jquery-3.4.1.min.js" type="application/javascript"></script>
+<script src="js/popper.min.js" type="application/javascript"></script>
+<script src="bootstrap-material/js/bootstrap-material-design.min.js" type="application/javascript"></script>
+
+¡Hola, mundo!
