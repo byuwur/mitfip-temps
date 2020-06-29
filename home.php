00a885a36cc276d865f7dc3bddd2740877862679
Nestor Portela <nportela29@itfip.edu.co>
2020-06-29 01:36:22 -0500

    Home del usuario
    Se crea el archivo home.php del usuario

---
+++ home.php
@@ -0,0 +1,44 @@
+<link href="https://mitfipespinal.comunisoft.com/bootstrap-material/css/bootstrap-material-design.css" rel="stylesheet" />
+<link href="https://mitfipespinal.comunisoft.com/css/main.css" rel="stylesheet" />
+<link href="https://mitfipespinal.comunisoft.com/fontawesome/css/all.min.css" rel="stylesheet" />
+<script src="https://mitfipespinal.comunisoft.com/js/jquery-3.4.1.min.js" type="application/javascript"></script>
+<script src="https://mitfipespinal.comunisoft.com/js/popper.min.js" type="application/javascript"></script>
+<script src="https://mitfipespinal.comunisoft.com/bootstrap-material/js/bootstrap-material-design.min.js" type="application/javascript"></script>
+<link href="fontawesome/css/all.min.css" rel="stylesheet" />
+<!-- MITFTIP custom CSS-JS 
+<link href="bootstrap-material/css/bootstrap-material-design.css" rel="stylesheet" />
+<link href="css/main.css" rel="stylesheet" />
+<script src="js/jquery-3.4.1.min.js" type="application/javascript"></script>
+<script src="js/popper.min.js" type="application/javascript"></script>
+<script src="bootstrap-material/js/bootstrap-material-design.min.js" type="application/javascript"></script -->
+<title>Home</title>
+<div class="text-center">
+    <h1><i class="fas fa-users"></i>MI PERFIL</h1>
+
+    <div class="row form-padding form-top-border">
+        <div class="col-4">
+            <small>Cédula:</small><br><b>1000111000</b>
+        </div>
+        <div class="col-4">
+            <small>Usuario:</small><br><b>USERNAME</b>
+        </div>
+        <div class="col-4">
+            <small>Nombre:</small><br><b>NOMBRE</b>
+        </div>
+        </div>
+
+    </div>
+    <div class="text-center">
+        <div class="row form-padding form-top-border">
+            <div class="col-sm-4">
+                <a class="btn btn-outline-success btn-block" href="#"><i class="fas fa-bell" style="font-size:24px"></i>Notificaciones</a>
+            </div>
+            <div class="col-sm-4">
+                <a class="btn btn-outline-info btn-block" href="adminedit.php?user=USERNAME"><i class="fas fa-user-edit" style="font-size:24px"></i>Editar Perfil</a>
+            </div>
+            <div id="deldiv_USERNAME" name="deldiv_USERNAME" class="col-sm-4">
+                <a class="btn btn-outline-warning btn-block"><i class="fas fa-sign-out-alt" style="font-size:24px"></i>Cerrar sesión</a>
+            </div>
+        </div>
+    </div>
+</div>
